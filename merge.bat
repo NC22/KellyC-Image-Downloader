421d706ae70ece2c8c4cdce81aa9230db7355333
Vladimir <torrenttvi@gmail.com>
2018-12-23 15:16:28 +0300

    Few avoid notices

--- merge.bat
+++ merge.bat
@@ -4,33 +4,35 @@ SET frontend=khelper.user.js
 SET background=khelper.bg.js
 SET debug=0
 
-echo.>%frontend%
+break>%frontend%
+:: or echo.>%frontend% to clear file before write
+copy %frontend% + "%~dp0\SIGN" %frontend%
 
 :: for \r 
 for %%x in (
-    "%~dp0\widget\kellyTooltip.js"  
-    "%~dp0\widget\kellyTileGrid.js" 
-    "%~dp0\widget\kellyImageView.js" 
-    "%~dp0\lib\kellyLoc.js"	
-    "%~dp0\lib\kellyStorageManager.js"	 
-    "%~dp0\lib\kellyThreadWork.js" 
-    "%~dp0\lib\kellyGrabber.js" 
-    "%~dp0\lib\kellyFastSave.js" 
-    "%~dp0\lib\kellyTools.js"
-    "%~dp0\lib\kellyOptions.js"
-    "%~dp0\lib\kellyFavItems.js"
-    "%~dp0\env\profiles\joyreactor.js" 
+    "widget\kellyTooltip.js"  
+    "widget\kellyTileGrid.js" 
+    "widget\kellyImageView.js" 
+    "lib\kellyLoc.js"	
+    "lib\kellyStorageManager.js"	 
+    "lib\kellyThreadWork.js" 
+    "lib\kellyGrabber.js" 
+    "lib\kellyFastSave.js" 
+    "lib\kellyTools.js"
+    "lib\kellyOptions.js"
+    "lib\kellyFavItems.js"
+    "env\profiles\joyreactor.js" 
 ) do (
 
     @echo.>> %frontend%
     @echo.>> %frontend%
     @echo.>> %frontend%
-    @echo //%%~x>> %frontend%
+    @echo // EXTRACTED FROM FILE %%~x>> %frontend%
     @echo.>> %frontend%
     @echo.>> %frontend%
     @echo.>> %frontend%
     
-    copy %frontend% + "%%~x" %frontend%
+    copy %frontend% + "%~dp0\%%~x" %frontend%
 )
 
 @echo // initialization>> %frontend%
@@ -46,21 +48,23 @@ copy %frontend% + "%~dp0\init.js" %frontend%
 @echo.>> %frontend%
 @echo // end of file >> %frontend%
 
-echo.>%background%
+break>%background%
+copy %background% + "%~dp0\SIGN" %background%
+
 for %%x in ( 
-    "%~dp0\lib\kellyTools.js" 
-    "%~dp0\lib\kellyDispetcher.js" 
+    "lib\kellyTools.js" 
+    "lib\kellyDispetcher.js" 
 ) do (
     
     @echo.>> %background%
     @echo.>> %background%
     @echo.>> %background%
-    @echo //%%~x>> %background%
+    @echo // EXTRACTED FROM FILE %%~x>> %background%
     @echo.>> %background%
     @echo.>> %background%
     @echo.>> %background%
     
-    copy %background% + "%%~x" %background%
+    copy %background% + "%~dp0\%%~x" %background%
 )
 
 @echo // initialization>> %background%
