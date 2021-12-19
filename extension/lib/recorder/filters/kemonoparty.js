6273fda8f1ff554e43fbb5faf69bb0c7fcfe8133
nradiowave <torrenttvi@gmail.com>
2021-12-20 00:23:56 +0300

    upd filter for kemono

--- extension/lib/recorder/filters/kemonoparty.js
+++ extension/lib/recorder/filters/kemonoparty.js
@@ -1,13 +1,27 @@
 KellyRecorderFilterKemono = new Object();
-KellyRecorderFilterKemono.manifest = {host : 'kemono.party', detectionLvl : ['imagePreview', 'imageOriginal']};
-KellyRecorderFilterKemono.addItemByDriver = function(handler, data) {
-    if (handler.url.indexOf('kemono.party') != -1 && data.el.tagName == 'IMG' && data.el.src.indexOf('/thumbnail/files/') != -1) {
-        
-       handler.addSingleSrc(item, data.el.src, 'addSrcFromAttributes-src', data.el, 'imagePreview');
-       handler.addSingleSrc(item, data.el.src.replace('/thumbnail', ''), 'addSrcFromAttributes-src', data.el, 'imageOriginal');
-       
-       return item.relatedSrc.length > 0 ? handler.addDriverAction.ADD : handler.addDriverAction.SKIP;            
-    } 
+KellyRecorderFilterKemono.manifest = {host : 'kemono.party', detectionLvl : ['imagePreview', 'imageOriginal', 'imageByDocument']};
+
+KellyRecorderFilterKemono.parseImagesDocByDriver = function(handler, data) {
+    
+    if (handler.url.indexOf('kemono.party') == -1 && data.thread.response) return;
+    
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(data.thread.response, 'text/html');
+    
+    var images = doc.querySelectorAll('.fileThumb');
+    for (var i = 0; i < images.length; i++) {
+        var href = KellyTools.getLocationFromUrl(images[i].href).pathname;
+        if (href && href.indexOf('/data/') === 0) handler.imagesPool.push({relatedSrc : ['https://kemono.party' + href]});
+    }
+
+    data.thread.response = ''; 
+    return true;
 }
+
+KellyPageWatchdog.validators.push({
+    url : 'kemono.party', 
+    host : 'kemono.party', 
+    patterns : [['/thumbnail/', 'imagePreview']]
+});
 
 KellyPageWatchdog.filters.push(KellyRecorderFilterKemono);
